19ab3690ddfeccfb129dd4dc01c633a18c2411fe
CoryRoth <cgroth@iastate.edu>
2025-03-13 15:45:16 -0500

    added second script to add sounds another way

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+	[SerializeField] public AudioClip plantSound; //Audio test
+	[SerializeField] public AudioClip harvestSound; //Audio test
+	[SerializeField] public AudioClip deathSound; //
+	[SerializeField] public AudioClip trapSound; //Audio test
+	[SerializeField] public AudioClip placeSound; //Audio test
+	[SerializeField] public AudioClip shopSound; //Audio test
+
+}
