ef806e72508f78cbab220e4e72027a0e0a7e4b21
zach <zntrpi@gmail.com>
2025-04-03 19:31:30 -0500

    Modified seed spawning

---
+++ Assets/Scripts/SeedSpawning/SpawnSeedPink.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SpawnSeedPink : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public float probability = 0.3f;
+
+    void Start()
+    {
+        SpawnSeeds();
+    }
+
+    void SpawnSeeds(){
+        foreach (Transform point in spawnPoints){
+            if(Random.value < probability){
+                ItemObject.CreateItemObject(point.position, new Item{itemType=Item.ItemType.FlowerPink});
+            }
+        }
+    }
+}
+
