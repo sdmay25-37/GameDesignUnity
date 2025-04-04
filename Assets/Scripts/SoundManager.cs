deef9b4efb33075e486cff498e78d387ac14b011
CoryRoth <cgroth@iastate.edu>
2025-04-03 19:26:05 -0500

    adding sound scripts

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    public float volume;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySFX(AudioClip clip, float volume = 1.0f)
+    {
+        if (clip != null)
+        {
+            volume = PlayerPrefs.GetFloat("Volume", 0.5f);
+            sfxSource.PlayOneShot(clip, volume);
+        }
+    }
+
+    public void PlayMusic(AudioClip clip, float volume = 1.0f, bool loop = true)
+    {
+        if (musicSource.clip == clip) return;
+
+        volume = PlayerPrefs.GetFloat("Volume", 0.5f);
+        musicSource.clip = clip;
+        musicSource.volume = volume;
+        musicSource.loop = loop;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+}
+
