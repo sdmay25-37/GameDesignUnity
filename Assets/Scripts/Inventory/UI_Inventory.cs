709ccd9ef10bdf891a6253cc5243d34887564d28
SpencerToddT <spencertodd02@outlook.com>
2025-04-16 19:59:24 -0500

    Inventory Adjusted
    Inventory adjust for all rescaling and improved readability

--- Assets/Scripts/Inventory/UI_Inventory.cs
+++ Assets/Scripts/Inventory/UI_Inventory.cs
@@ -67,7 +67,7 @@ public class UI_Inventory : MonoBehaviour
 
         int x = 0;
         int y = 0;
-        float itemSlotCellSize = 55f;
+        float itemSlotCellSize = 100f;
         foreach (Item item in inventory.GetItemList()){
             if (!init) locate();
             RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
